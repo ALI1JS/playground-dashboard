ea269952bf4e406696624f5d8b1d0ffb60676c51
alijs <aliismailh0@gmail.com>
2024-04-17 08:58:24 -0700

    first version

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     
+        <div className='w-[100px] bg-black'>
+            <h2>welcome</h2>
+        </div>
+    </>
+  )
+}
+
+export default App
